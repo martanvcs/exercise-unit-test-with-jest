94bccf2d35bb85428fe655de65a2279f48a68208
Grin <martanvcs@gmail.com>
2025-04-22 07:03:34 +0000

    primer commit

---
+++ .learn/resets/11-homework/app.js
@@ -0,0 +1,29 @@
+const fromEuroToDollar = function(valueInEuro) {
+    // Convertimos el valor a dólares
+    let valueInDollar = valueInEuro * 1.07;
+    // Retornamos el valor en dólares
+    return valueInDollar;
+}
+
+const fromDollarToYen = function(valueInDollar) {
+    let valueInYen = valueInDollar * 156.5;
+    return valueInYen;
+}
+
+const fromYenToPound = function(valueInYen) {
+    let valueInPound = valueInYen * 0.87;
+    return valueInPound;
+}
+
+const sum =(a,b) => {
+    return a + b
+}
+
+console.log(sum(7,3))
+
+module.exports = {sum};
+
+
+module.exports = { sum, fromEuroToDollar }
+module.exports = { sum, fromDollarToYen }
+module.exports = { sum, fromYenToPound}
---
+++ test.js
@@ -0,0 +1,48 @@
+const{sum, fromYenToPound}= require('./app.js');
+
+test('adds 14 + 9 to equal 23',() => {
+    let total = sum(14,9);
+    expect(total).toBe(23); 
+});
+
+test("One euro should be 1.07 dollars", function() {
+    // Importo la funcion desde app.js
+    const { fromEuroToDollar } = require('./app.js');
+
+    // Uso la función como debe ser usada
+    const dollars = fromEuroToDollar(3.5);
+
+    // Si 1 euro son 1.07 dólares, entonces 3.5 euros debe ser (3.5 * 1.07)
+    const expected = 3.5 * 1.07;
+
+    // Hago mi comparación (la prueba)
+    expect(fromEuroToDollar(3.5)).toBe(3.745); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
+})
+
+test("One dollar should be 156.5 yenes", function() {
+    
+    const { fromDollarToYen } = require('./app.js');
+
+   
+    const yens = fromDollarToYen(3.5);
+
+   
+    const expected = 3.5 * 156.5;
+
+   
+    expect(fromDollarToYen(3.5)).toBe(547.75); 
+})
+
+test("One yen should be 0.87 pounds", function() {
+    
+    const { fromYenToPound } = require('./app.js');
+
+    
+    const pounds = fromYenToPound(3.5);
+
+    
+    const expected = 3.5 * 0.87;
+
+    
+    expect(fromYenToPound(3.5)).toBe(3.045); 
+})
